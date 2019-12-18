68ce55a10f418d847185dc8fab8cd7f70422d821
vikas jain <vkasjn2193@gmail.com>
2019-12-18 10:55:00 +0530

    Removed unnecesary space

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,30 @@
-const weatherForm  = document.querySelector('form')
-const search       = document.querySelector('input')
-const messageOne   = document.querySelector('#message-1')
-const messageTwo   = document.querySelector('#message-2')
+const weatherForm = document.querySelector('form')
+const search      = document.querySelector('input')
+const messageOne  = document.querySelector('#message-1')
+const messageTwo  = document.querySelector('#message-2')
 // messageOne.textContent='from javascript'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
-messageOne.textContent='Loading...'
-messageTwo.textContent=''
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
 
     fetch('http://localhost:3000/weather?address= ' + location).then((response) => {
         response.json().then((data) => {
-            if (data.error)
-             {
+            if (data.error) {
                 // console.log(data.error)
-   messageOne.textContent=data.error
+                messageOne.textContent = data.error
             }
 
             else {
 
-                 // console.log(data.location)
+                // console.log(data.location)
                 // console.log(data.forecast)
-                messageOne.textContent=data.location
-                messageTwo.textContent=data.forecast
-               
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+
             }
         })
     })
